15806a550e03f604c4ae3a762b63dd4534a7e604
Josepogers <zekreds12@gmail.com>
2026-02-16 18:25:37 -0300

    fix bug to use correct .env

--- bot_instance.py
+++ bot_instance.py
@@ -1,5 +1,9 @@
+from settings import env_file
+from dotenv import load_dotenv
 from os import getenv
 import telebot
+
+load_dotenv(dotenv_path=env_file, override=True)
 
 bot_api_key = getenv("bot_api_key")
 
